709058410f7a3378366650367cbcf5618949e15d
nommy <asuinim@gmail.com>
2022-09-16 15:50:44 +0900

    Create create_ports_table.php
    impl pre-create_ports_table.php

---
+++ database/migrations/create_ports_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('term_id');
+            $table->string('taxonomy');
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('term_group');
+            $table->integer('term_order');
+            $table->unsignedBigInteger('term_taxonomy_id');
+            $table->text('description');
+            $table->unsignedBigInteger('parent');
+            $table->unsignedBigInteger('count');
+            $table->unsignedBigInteger('nationality');
+            $table->string('mmsi');
+            $table->string('imo');
+            $table->string('url');
+            $table->unsignedBigInteger('company');
+            $table->unsignedBigInteger('manufactor');
+            $table->string('english-name');
+            $table->string('active');
+            $table->date('launched');
+            $table->date('start-date');
+            $table->string('military-number');
+            $table->string('callsign');
+            $table->string('urltitle');
+            $table->string('class');
+            $table->unsignedBigInteger('history')->nullable(true);
+            $table->unsignedBigInteger('ship_type');
+            $table->integer('history_poa')->nullable(true);
+            $table->text('memo')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ships');
+    }
+}
